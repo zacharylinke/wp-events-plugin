afefc60d340d575e40ae3138f7ca6d35d5b8c9a8
Zachary Linke <zlinke77@gmail.com>
2014-05-05 15:34:56 -0400

    Initial plugin file setup

---
+++ upcoming-events.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: Upcoming Events
+* Plugin URI: http://zacharylinke.com
+* Descritpion: A plugin to create events with repeating date ability
+* Version: 1.0
+* Author: Zachary Linke
+* Author URI: http://zacharylinke.com
+* License: GPL2
+*/
