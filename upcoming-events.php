bce03b00353eb588b2c1d798013215a5fe18cca2
Zachary Linke <zlinke77@gmail.com>
2014-05-06 10:33:52 -0400

    register event post type

--- upcoming-events.php
+++ upcoming-events.php
@@ -8,3 +8,49 @@
 * Author URI: http://zacharylinke.com
 * License: GPL2
 */
+
+// DEFINE PATH CONSTANTS
+define( 'ROOT', plugins_url( '', __FILE__ ) );
+define( 'IMAGES', ROOT . '/img/' );
+define( 'STYLES', ROOT . '/css/' );
+define( 'SCRIPTS', ROOT . '/js/' );
+
+function uep_custom_post_type() {
+	// 'EVENT' POST TYPE DATA
+	// Labels for events
+	$labels = array(
+	    'name'                  =>   __( 'Events', 'uep' ),
+	    'singular_name'         =>   __( 'Event', 'uep' ),
+	    'add_new_item'          =>   __( 'Add New Event', 'uep' ),
+	    'all_items'             =>   __( 'All Events', 'uep' ),
+	    'edit_item'             =>   __( 'Edit Event', 'uep' ),
+	    'new_item'              =>   __( 'New Event', 'uep' ),
+	    'view_item'             =>   __( 'View Event', 'uep' ),
+	    'not_found'             =>   __( 'No Events Found', 'uep' ),
+	    'not_found_in_trash'    =>   __( 'No Events Found in Trash', 'uep' )
+	);
+	 
+	// Default fields for events
+	$supports = array(
+	    'title',
+	    'editor',
+	    'excerpt'
+	);
+	 
+	// Arguments for events
+	$args = array(
+	    'label'         =>   __( 'Events', 'uep' ),
+	    'labels'        =>   $labels,
+	    'description'   =>   __( 'A list of upcoming events', 'uep' ),
+	    'public'        =>   true,
+	    'show_in_menu'  =>   true,
+	    'menu_icon'     =>   IMAGES . 'event.svg',
+	    'has_archive'   =>   true,
+	    'rewrite'       =>   true,
+	    'supports'      =>   $supports
+	);
+
+	// Register event post type
+	register_post_type( 'event', $args );	
+}
+add_action( 'init', 'uep_custom_post_type' );
